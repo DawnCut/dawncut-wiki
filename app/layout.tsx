d7d407e20be48e23997472571cf5e48c425c24aa
Livezxy <47280313+Livezxy@users.noreply.github.com>
2025-03-29 00:13:33 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout({children}) {
             // banner={banner}
             navbar={navbar}
             pageMap={await getPageMap()}
-            docsRepositoryBase="https://github.com/DawnCut/dawncut-wiki"
+            docsRepositoryBase="https://github.com/DawnCut/dawncut-wiki/tree/main"
             footer={footer}
             // ... Your additional layout options
         >
